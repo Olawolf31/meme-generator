2f4847d7b942ee7c7498eb097dc4b99ef0507333
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-01-03 22:52:21 +0100

    updated title

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                     <div className="top">
                         <img src={logo} width='50px' alt='' /> 
                         <div className='header-title'>MemeGenerator</div>
-                        <div className='header-title2'>React Course - Project 3</div>
+                        <div className='header-title2'>Create Fun Memes</div>
                     </div>
                 </section>
             </header>
